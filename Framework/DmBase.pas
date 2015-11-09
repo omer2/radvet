6367af5fc86b67adaa056f01f5ccdeb5b7dfaf8c
omer2 <omerayverdi@gmail.com>
2015-11-09 18:04:52 +0800

    Create DmBase.pas

---
+++ Framework/DmBase.pas
@@ -0,0 +1,23 @@
+unit DmBase;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs;
+
+type
+  TDtmBase = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DtmBase: TDtmBase;
+
+implementation
+
+{$R *.DFM}
+
+end.
